a2518487308e2eaf3648f8a47fbf93c357e07ebb
Eonum Inc <56983425+Eeonum@users.noreply.github.com>
2020-09-13 17:17:24 -0700

    Add files via upload
    Chapter1: Approach to QFT

---
+++ chapter 1: Approach to Quantum Field Theory.py
@@ -0,0 +1,20 @@
+from sympy import symbols, Sum, Indexed, lambdify, Array
+from sympy.physics.quantum import Ket
+
+import numpy as np
+
+n, nu = symbols("n nu")
+N = Sum(Indexed('n', nu), (nu, 0, 3))
+f = lambdify(n, N)
+b = []
+n_nu = []
+for nu in range(1, 5):
+    arr1 = np.array([nu])
+    b = np.concatenate((b, arr1))
+    arr2 = Indexed('n', nu)
+    n_nu = np.concatenate((n_nu, Array(arr2)))
+
+print(f(b))
+
+ket_n_nu = Ket(n_nu)
+print(ket_n_nu)
